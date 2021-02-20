247adb24180a87e0ebd96be1a7e7e8007ec02322
zhaoyingjie <zhaoyingjie@xiaodongai.com>
2021-02-20 09:46:10 +0800

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'zyjie9426/imports',
+  ),
+  'versions' => 
+  array (
+    'zyjie9426/imports' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
